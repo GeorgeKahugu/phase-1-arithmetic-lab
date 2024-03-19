687f76bd782070848888e552e2891745ae87be87
GeorgeKahugu <gee.kahugu@gmail.com>
2024-03-19 22:35:49 +0300

    Arithmetic Operators

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 
-// Write your code here
+var num1=31;
+var num2=2;
+var multiply= num1*num2
+console.log(multiply);
+
+
+var random =(1,10)
+console.log(random);
+
+
+const num3 = 24;
+const num4 = 5;
+const mod = num3 % num4;
+console.log(mod);
+
+
+
+const numbers = (9,10,13,15,18,20)
+const max= Math.max (9,10,13,15,18,20)
+console.log(max);
+
+
